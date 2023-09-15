df0bc4e9fd748524ce1e0ae6ad97ced3ba094449
Alex Suraci <suraci.alex@gmail.com>
2023-09-15 09:41:59 -0400

    add apko module

---
+++ apko/apko.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"runtime"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Apko struct{}
+
+func (Apko) Alpine(ctx context.Context, packages []string) (*Container, error) {
+	ic := baseConfig()
+	ic["contents"] = cfg{
+		"repositories": []string{
+			"https://dl-cdn.alpinelinux.org/alpine/edge/main",
+		},
+		"packages": append([]string{"alpine-base"}, packages...),
+	}
+	return apko(ic)
+}
+
+func (Apko) Wolfi(ctx context.Context, packages []string) (*Container, error) {
+	ic := baseConfig()
+	ic["contents"] = cfg{
+		"repositories": []string{
+			"https://packages.wolfi.dev/os",
+		},
+		"keyring": []string{
+			"https://packages.wolfi.dev/os/wolfi-signing.rsa.pub",
+		},
+		"packages": append([]string{"wolfi-base"}, packages...),
+	}
+	return apko(ic)
+}
+
+type cfg map[string]any
+
+func baseConfig() cfg {
+	return cfg{
+		"cmd": "/bin/sh",
+		"environment": cfg{
+			"PATH": "/usr/sbin:/sbin:/usr/bin:/bin",
+		},
+		"archs": []string{runtime.GOARCH},
+	}
+}
+
+func apko(cfg any) (*Container, error) {
+	cfgYAML, err := yaml.Marshal(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return dag.Container().Import(
+		dag.Container().
+			From("cgr.dev/chainguard/apko").
+			WithMountedFile(
+				"/config.yml",
+				dag.Directory().
+					WithNewFile("config.yml", string(cfgYAML)).
+					File("config.yml"),
+			).
+			WithDirectory("/layout", dag.Directory()).
+			WithMountedCache("/apkache", dag.CacheVolume("apko")).
+			WithExec([]string{
+				"build",
+				"--cache-dir", "/apkache",
+				"/config.yml", "latest", "/layout.tar",
+			}).
+			File("/layout.tar"),
+	), nil
+}
---
+++ apko/go.mod
@@ -0,0 +1,15 @@
+module apko
+
+go 1.21.0
+
+require (
+	github.com/99designs/gqlgen v0.17.31
+	github.com/Khan/genqlient v0.6.0
+	golang.org/x/sync v0.3.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
+)
