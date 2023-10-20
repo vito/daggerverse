8ca2747d583e685b6ee368409500da99942e6b24
Alex Suraci <suraci.alex@gmail.com>
2023-10-20 12:22:11 -0400

    apply defaults at runtime as workaround

--- concourse/main.go
+++ concourse/main.go
@@ -9,20 +9,30 @@ import (
 type Concourse struct{}
 
 type QuickstartOpts struct {
-	// Image   string `doc:"concourse image tag" default:"concourse/concourse:7.10@sha256:e45dffda72e32e11e5790530f8b41a23af4a49a21d585967c4f00c3cf3b12164"`
-	// containerd is not working on macOS when using the official image
-	Image   string `doc:"concourse image tag" default:"rdclda/concourse:7.9.1@sha256:963929885c7a9b917dfd98244757b4f32f64b3c322c842a2da20aca6e32787b2"`
-	DBImage string `doc:"concourse db image tag" default:"postgres:15.4-alpine@sha256:6f5520d31e1223facb11066b6c99333ffabf190a5d48c50d615b858602f5f8b5"`
+	Image   string `doc:"concourse image tag" default:"concourse/concourse:7.10@sha256:e45dffda72e32e11e5790530f8b41a23af4a49a21d585967c4f00c3cf3b12164"`
 	WebPort int    `doc:"concourse web port" default:"8080"`
+	DBImage string `doc:"concourse db image tag" default:"postgres:15.4-alpine@sha256:6f5520d31e1223facb11066b6c99333ffabf190a5d48c50d615b858602f5f8b5"`
+	DBName  string `doc:"concourse db name" default:"concourse"`
 }
 
 func (m *Concourse) Quickstart(ctx context.Context, opts QuickstartOpts) *Service {
+	// TODO: workaround dagger up CLI not respecting defaults
+	if opts.Image == "" {
+		opts.Image = "concourse/concourse:7.10@sha256:e45dffda72e32e11e5790530f8b41a23af4a49a21d585967c4f00c3cf3b12164"
+	}
+	if opts.DBImage == "" {
+		opts.DBImage = "postgres:15.4-alpine@sha256:6f5520d31e1223facb11066b6c99333ffabf190a5d48c50d615b858602f5f8b5"
+	}
+	if opts.WebPort == 0 {
+		opts.WebPort = 8080
+	}
+
 	workerWorkDir := dag.CacheVolume("concourse-worker-work-dir")
 
 	return dag.Container().From(opts.Image).
 		WithMountedCache("/concourse-worker-work-dir", workerWorkDir).
 		WithEnvVariable("CONCOURSE_WORKER_WORK_DIR", "/concourse-worker-work-dir").
-		WithServiceBinding("db", m.postgresql(ctx, opts.DBImage)).
+		WithServiceBinding("db", m.postgresql(ctx, opts.DBImage, opts.DBName)).
 		WithExposedPort(opts.WebPort).
 		WithEnvVariable("CONCOURSE_BIND_PORT", strconv.Itoa(opts.WebPort)).
 		WithEnvVariable("CONCOURSE_POSTGRES_HOST", "db").
@@ -46,13 +56,13 @@ func (m *Concourse) Quickstart(ctx context.Context, opts QuickstartOpts) *Servic
 		AsService()
 }
 
-func (m *Concourse) postgresql(ctx context.Context, image string) *Service {
+func (m *Concourse) postgresql(ctx context.Context, image, dbName string) *Service {
 	return dag.Container().From(image).
 		WithExposedPort(5432).
 		WithEnvVariable("POSTGRES_DB", "concourse").
 		WithEnvVariable("POSTGRES_USER", "concourse").
 		WithEnvVariable("POSTGRES_PASSWORD", "concourse").
 		WithEnvVariable("PGDATA", "/database").
-		WithMountedCache("/database", dag.CacheVolume("concourse-db")).
+		WithMountedCache("/database", dag.CacheVolume(fmt.Sprintf("concourse-db-%s", dbName))).
 		AsService()
 }
