4809c149d0ed114dc199bc17cfec6b51e4f27858
Alex Suraci <suraci.alex@gmail.com>
2024-02-08 08:46:48 -0500

    add encapsulation test

--- test/main.go
+++ test/main.go
@@ -1,3 +1,15 @@
 package main
 
+import "context"
+
 type Main struct{}
+
+func (m *Main) Encapsulate(ctx context.Context) error {
+	_ = m.Fail(ctx)
+	return nil
+}
+
+func (*Main) Fail(ctx context.Context) error {
+	_, err := dag.Container().From("nixos/nix").WithExec([]string{"false"}).Sync(ctx)
+	return err
+}
