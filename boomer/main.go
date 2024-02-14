0382faefcb21962f762da9c43f60f0e445c33535
Alex Suraci <suraci.alex@gmail.com>
2024-02-14 10:54:21 -0500

    ok boomer

---
+++ boomer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/Khan/genqlient/graphql"
+)
+
+type Boomer struct {
+}
+
+// Boom keeps calling itself with a larger and larger n until it booms.
+func (m *Boomer) Boom(
+	ctx context.Context,
+	// +optional
+	// +default=1
+	n int,
+) error {
+	return dag.c.MakeRequest(ctx, &graphql.Request{
+		Query: "query Dig($x: Int!){boomer{boom(n: $x)}}",
+		Variables: map[string]interface{}{
+			"x": n + 1,
+		},
+	}, &graphql.Response{})
+}
